24f6fb5bbb689ed768c429b3a3a30de129894488
Helltar <iam@helltar.com>
2026-02-06 15:03:58 +0200

    add REQUEST_FAILED constant and replace triple-quoted strings with regular strings in Strings object

--- src/main/kotlin/Strings.kt
+++ src/main/kotlin/Strings.kt
@@ -1,6 +1,7 @@
 object Strings {
 
-    const val HOW_TO_USE = """ℹ️ How to use: <code>/cur 1 usd to uah</code>"""
-    const val INVALID_QUERY = """⚠️ Please enter a query between 8 and 64 characters"""
-    const val PARSING_ERROR = """❌ Error when parsing google response"""
+    const val HOW_TO_USE = "ℹ️ How to use: <code>/cur 1 usd to uah</code>"
+    const val INVALID_QUERY = "⚠️ Please enter a query between 8 and 64 characters"
+    const val PARSING_ERROR = "❌ Error when parsing google response"
+    const val REQUEST_FAILED = "❌ Request failed, please try again later"
 }
