55cb4ebbc70b13dc0b1a4c221de163ea5ef12c7c
Helltar <iam@helltar.com>
2026-02-06 15:04:47 +0200

    refactor config to reuse dotenv instance and rename readEnvVar to readEnv

--- src/main/kotlin/Config.kt
+++ src/main/kotlin/Config.kt
@@ -2,14 +2,13 @@ import io.github.cdimascio.dotenv.dotenv
 
 object Config {
 
-    val creatorId =
-        readEnvVar("CREATOR_ID").toLongOrNull()
-            ?: throw IllegalArgumentException("Invalid value for CREATOR_ID")
+    private val dotenv = dotenv { ignoreIfMissing = true }
 
-    val telegramBotToken = readEnvVar("BOT_TOKEN")
-    val telegramBotUsername = readEnvVar("BOT_USERNAME")
+    val creatorId = readEnv("CREATOR_ID").toLongOrNull() ?: throw IllegalArgumentException("Invalid value for CREATOR_ID")
+    val telegramBotToken = readEnv("BOT_TOKEN")
+    val telegramBotUsername = readEnv("BOT_USERNAME")
 
-    private fun readEnvVar(env: String) =
-        dotenv { ignoreIfMissing = true }[env].ifBlank { throw IllegalArgumentException("$env environment variable is blank") }
+    private fun readEnv(env: String) =
+        dotenv[env].ifBlank { throw IllegalArgumentException("$env environment variable is blank") }
             ?: throw IllegalArgumentException("$env environment variable is missing")
 }
